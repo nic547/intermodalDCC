e67075e93f5142df27fdaf9ca596eb4d66e5552b
nic547 <noreply@nic547.ch>
2025-03-14 19:43:27 +0100

    Send regular reminders to dcc decoders

--- firmware/src/state_manager.hpp
+++ firmware/src/state_manager.hpp
@@ -4,7 +4,7 @@
 #include <signal_generation/signal_generation.hpp>
 
 namespace state_manager {
-  void process();
-  void setFunction(uint16_t address, uint8_t number, bool on);
-  void setSpeed(uint16_t address, uint8_t speed, bool forwards);
+void process();
+void setFunction(uint16_t address, uint8_t number, bool on);
+void setSpeed(uint16_t address, uint8_t speed, bool forwards);
 }
