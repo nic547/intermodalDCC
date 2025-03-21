542d3d68a0925ff9816b0dd8d901488aa884c7e5
nic547 <7825979+nic547@users.noreply.github.com>
2025-03-22 00:05:00 +0100

    further work on editing engines

--- web/src/app/engine/engine-root.component.ts
+++ web/src/app/engine/engine-root.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { StateService } from '../services/state-service/state.service';
-import { DccFunction, SimpleEngine } from './types';
+import { DccFunction, PersistenEngine, SimpleEngine } from './types';
 import { NgFor, NgIf } from '@angular/common';
 import { EngineSelectionComponent } from './engine-selection/engine-selection.component';
 import { EngineEditorComponent } from "./engine-editor/engine-editor.component";
 import { EngineControllerComponent } from './engine-controller/engine-controller.component';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-engine-root',
@@ -18,12 +19,17 @@ export class EngineRootComponent {
 
   protected showSelection = signal(false);
 
-  async addSimpleEngine() {
+  async createSimpleEngine() {
     let engine = new SimpleEngine();
     for (let i = 0; i <= 28; i++) {
       engine.functions.push(DccFunction.create(i));
     }
     await this.stateService.activateEngine(engine);
+  }
+
+  async createPersistentEngine() {
+    let engine = new PersistenEngine();
+    this.stateService.editingEngine.set(engine);
   }
 
   async selectEngine() {
