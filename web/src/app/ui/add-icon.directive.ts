2aa63c3dade861fafeda71bfcb9d2c146c40dbf3
nic547 <7825979+nic547@users.noreply.github.com>
2025-05-25 23:52:22 +0200

    fix svg icons not respecting text color

--- web/src/app/ui/add-icon.directive.ts
+++ web/src/app/ui/add-icon.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 export class AddIconDirective {
 
   constructor (elementRef: ElementRef, renderer: Renderer2) {
-    renderer.setProperty(elementRef.nativeElement, 'innerHTML', '<svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>');
+    renderer.setProperty(elementRef.nativeElement, 'innerHTML', '<svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>');
     renderer.addClass(elementRef.nativeElement, 'svg');
   }
 
