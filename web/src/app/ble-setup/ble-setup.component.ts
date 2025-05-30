c9b25ec4befcb441f96c6459e11ec3b4a65ca1da
nic547 <7825979+nic547@users.noreply.github.com>
2025-05-30 19:55:32 +0200

    Upgrade to Angular 20 and apply migrations

--- web/src/app/ble-setup/ble-setup.component.ts
+++ web/src/app/ble-setup/ble-setup.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { inject } from '@angular/core';
-import { NgIf } from '@angular/common';
+
 import BLEServiceToken from '../services/ble-service/ble.interface';
 
 @Component({
   selector: 'app-ble-setup',
-  imports: [NgIf],
+  imports: [],
   templateUrl: './ble-setup.component.html',
   styleUrl: './ble-setup.component.css'
 })
