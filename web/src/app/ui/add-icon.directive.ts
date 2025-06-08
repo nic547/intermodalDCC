92c7f66bf867fe3b8325bf8fe530d999dfe23aa7
nic547 <7825979+nic547@users.noreply.github.com>
2025-06-08 13:23:49 +0200

    web: fix icon directives
    constructor injection doesn't play nice with the 'import type' rules, but I can just use the newer inject() stuff.

--- web/src/app/ui/add-icon.directive.ts
+++ web/src/app/ui/add-icon.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, type ElementRef, type Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, inject } from '@angular/core';
 
 @Directive({
     selector: '[appAddIcon]',
 })
 export class AddIconDirective {
-    constructor(elementRef: ElementRef, renderer: Renderer2) {
-        renderer.setProperty(
-            elementRef.nativeElement,
+    private readonly elementRef = inject(ElementRef);
+    private readonly renderer = inject(Renderer2);
+
+    constructor() {
+        this.renderer.setProperty(
+            this.elementRef.nativeElement,
             'innerHTML',
             '<svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>',
         );
-        renderer.addClass(elementRef.nativeElement, 'svg');
+        this.renderer.addClass(this.elementRef.nativeElement, 'svg');
     }
 }
--- web/src/app/ui/arrow-forward-icon.directive.ts
+++ web/src/app/ui/arrow-forward-icon.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, type ElementRef, type Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, inject } from '@angular/core';
 
 @Directive({
     selector: '[appArrowForwardIcon]',
 })
 export class ArrowForwardIconDirective {
-    constructor(elementRef: ElementRef, renderer: Renderer2) {
-        renderer.setProperty(
-            elementRef.nativeElement,
+    private readonly elementRef = inject(ElementRef);
+    private readonly renderer = inject(Renderer2);
+
+    constructor() {
+        this.renderer.setProperty(
+            this.elementRef.nativeElement,
             'innerHTML',
             '<svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="currentColor"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>',
         );
-        renderer.addClass(elementRef.nativeElement, 'svg');
+        this.renderer.addClass(this.elementRef.nativeElement, 'svg');
     }
 }
