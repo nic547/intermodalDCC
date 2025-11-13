f1b5260c0eb8079508030ec4f31d0d942b94c86b
nic547 <noreply@nic547.ch>
2025-11-13 19:34:36 +0100

    Web: Try improving LM parsing performance
    - Adjust prompt to explicitly not generate other text. Some models like adding prose before the output
    - use /no_think specifically for qwen models that support thinking
    - temperature 0 because we need 0 creativity
    - filter out thinking and code blocks

--- web/src/app/services/llm-service/llm.service.ts
+++ web/src/app/services/llm-service/llm.service.ts
@@ -23,20 +23,24 @@ export class LlmService {
         messages: [
           {
             role: "system",
-            content: 'You will receive a text extracted from a pdf manual for a model train. This text should contain a descriptions of available dcc functions. The functions might be out of order.  Keep in mind that functions are generally referred to with a number and "F" as a prefix. F0 is usually the first function. Some functions might be described with icons, if you cannot identify a textual description ignore it. Functions might be described in multiple languages, if this is the case only return the German texts. Example output: [ { "f": 0, "d": "Licht"},{"f":1,"d":"Sound"}]'
+            content: 'You will receive a text extracted from a pdf manual for a model train. This text should contain a descriptions of available dcc functions. The functions might be out of order.  Keep in mind that functions are generally referred to with a number and "F" as a prefix. F0 is usually the first function. Some functions might be described with icons, if you cannot identify a textual description ignore it. Functions might be described in multiple languages, if this is the case only return the German texts. Example output: [ { "f": 0, "d": "Licht"},{"f":1,"d":"Sound"}] Only export the json array, no other text.'
           },
           {
             role: "user",
-            content: text
+            content: text + '/no_think'
           }
-        ]
+        ],
+        temperature: 0
       })
     });
 
     const responseObject = await result.json() as OAIChatCompletionResponse;
 
     if (responseObject) {
-      return JSON.parse(responseObject.choices[0].message.content) as LlmFunctionOutput[];
+      var outputText = responseObject.choices[0].message.content;
+      // Filter out thinking output and code blocks if any exist.
+      var filteredText = outputText.replace(/.*<\/think>/s, '').replace(/```json|```/g, '');
+      return JSON.parse(filteredText) as LlmFunctionOutput[];
     }
     else {
       return new Error("No response from LLM");
